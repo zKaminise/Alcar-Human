37432525c02d4ad02bcbcaf8ef7b14dc037c07f3
Gabriel Kaminise <gabriel.misao08@gmail.com>
2025-08-21 00:34:38 -0300

    Ajuste no from da API

--- api/contact.mjs
+++ api/contact.mjs
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     const resend = new Resend(process.env.RESEND_API_KEY);
 
     await resend.emails.send({
-      from: "Site <no-reply@seudominio.com.br>",
+      from: "Alcar Humana <onboarding@resend.dev>",
       to: process.env.TO_EMAIL,
       replyTo: email,
       subject: `Proposta | ${company} - ${name}`,
